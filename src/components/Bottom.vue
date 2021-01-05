8ceb66d76a412153eb3ade758becc3ec9af16bb9
Jecosine <mj_ol@outlook.com>
2021-01-05 08:06:59 +0800

    ADD: Add top and bottom navigation

--- src/components/Bottom.vue
+++ src/components/Bottom.vue
@@ -1,7 +1,50 @@
 <!--
  * @Author: Jecosine
  * @Date: 2021-01-05 07:52:23
- * @LastEditTime: 2021-01-05 07:52:31
+ * @LastEditTime: 2021-01-05 08:06:34
  * @LastEditors: Jecosine
  * @Description: Bottom navigation
 -->
+<template>
+  <div id="bottom-navigation">
+      
+  </div>
+</template>
+
+<script>
+export default {
+    name: "bottom",
+    props: {
+        config: Object
+    },
+    data() {
+        return {
+            defaltConfig: [
+                {
+                    "name": "Home",
+                    "icon": "el-icon-s-home"
+                },
+                {
+                    "name": "Files",
+                    "icon": "el-icon-s-management"
+                },
+                {
+                    "name": "Me",
+                    "icon": "el-icon-s-custom"
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+#bottom-navigation {
+    position: fixed;
+    width: 100%;
+    height: 60px;
+    bottom: 0;
+    left: 0;
+    background: violet;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,21 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <bottom />
   </div>
+  
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Bottom from '../components/Bottom.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Bottom
   }
 }
 </script>
